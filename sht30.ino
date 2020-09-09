a5379bebdc8d9764554e7d41bb0b36f3303176f7
uribo <ur1b0.dev@gmail.com>
2020-09-09 23:58:41 +0900

    温湿度を算出して渡すようにした

--- sht30.ino
+++ sht30.ino
@@ -18,8 +18,10 @@ void sht30_reset(){
   delay(10);
 }
 
-bool sht30_get_th( uint16_t *temp, uint16_t *hum){
+bool sht30_get_th( uint32_t *temp, uint32_t *hum){
   uint8_t buff[6] ={0};
+  uint32_t buff_temp = 0;
+  uint32_t buff_hum = 0;
 
   Wire.beginTransmission( suc_addr );
   Wire.write( command_highrep, sizeof( command_highrep ) );
@@ -37,8 +39,12 @@ bool sht30_get_th( uint16_t *temp, uint16_t *hum){
     buff[i] = Wire.read();
   }
 
-  *temp = (uint16_t)buff[0] << 8 | buff[1];
-  *hum = (uint16_t)buff[3] << 8 | buff[4];
+  buff_temp = (uint16_t)buff[0] << 8 | buff[1];
+  *temp = ( ( 17500 * buff_temp ) / ( ( 1 << 16 ) - 1) ) - 4500;
+
+
+  buff_hum = (uint16_t)buff[3] << 8 | buff[4];
+  *hum = ( 10000 * buff_hum ) / ( ( 1 << 16 ) - 1 );
 
   return true;
 }
