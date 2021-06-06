0187bc30ea3f67a6166e571db0eaa3d081c5ab33
uribo <ur1b0.dev@gmail.com>
2021-06-06 23:29:05 +0900

    フォーマッタをかけた

--- usb_temperature_humidity.ino
+++ usb_temperature_humidity.ino
@@ -2,25 +2,27 @@
 
 #define STATUS_OK 0
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
-  while (!Serial) {
+  while (!Serial)
+  {
     ; // wait for serial port to connect
   }
   delay(1000);
-  
-  sht30_init( SHT30_ADDR );
-  
+
+  sht30_init(SHT30_ADDR);
 }
 
-void loop() {
-  
+void loop()
+{
+
   // put your main code here, to run repeatedly:
   uint32_t ui_temp = 0;
   uint32_t ui_hum = 0;
 
-  sht30_get_th( &ui_temp, &ui_hum );
+  sht30_get_th(&ui_temp, &ui_hum);
 
   Serial.print("temp=");
   Serial.print(ui_temp, DEC);
@@ -30,5 +32,5 @@ void loop() {
   Serial.print(STATUS_OK);
   Serial.print("\n");
 
-  delay( 1000 );
+  delay(1000);
 }
